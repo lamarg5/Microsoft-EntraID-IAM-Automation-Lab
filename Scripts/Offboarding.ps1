c3ef510838d1cf388557eaab33188e3e6efdc877
lamarg5 <80973641+lamarg5@users.noreply.github.com>
2026-06-13 22:39:15 -0400

    Update Offboarding.ps1

--- Scripts/Offboarding.ps1
+++ Scripts/Offboarding.ps1
@@ -1,9 +1,8 @@
 Set-EntraUser `
-   -UserId 'john.smith@@company.onmicrosoft.com' `
+   -UserId 'john.smith@company.onmicrosoft.com' `
     -AccountEnabled $false
  $Group = Get-EntraGroup -Filter "displayName eq 'IT-HelpDesk-Team'"
 $User = Get-EntraUser -UserId 'john.smith@company.onmicrosoft.com
  Remove-EntraGroupMember `
      -GroupId $Group.Id `
     -MemberId $User.Id
-PS C:\Windows\System32>
